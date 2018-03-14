829d04cc95453836c138d506e1be3d27dfeb5c76
mattcroberts <mattroberts.uk@gmail.com>
2018-03-14 21:29:27 +0000

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# World Domination
-A work in progress game of strategic war campaigns.
-
-## Built with
-* React
-* Redux
-* Konva
--- src/setupTests.js
+++
@@ -1,8 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import chai from 'chai';
-import chaiEnzyme from 'chai-enzyme';
-
-chai.config.truncateThreshold = 0;
-chai.use(chaiEnzyme());
-configure({ adapter: new Adapter() });
