dfbba7772f8eda679109912f44124f3819a6348e
Matt Roberts <matt.roberts@sapient.com>
2018-11-20 12:58:51 +0000

    update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,31 @@
 module.exports = {
-    env: {
-        browser: true,
-        node: true,
-        es6: true,
-        jest: true
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    extends: 'eslint:recommended',
-    parserOptions: {
-        ecmaFeatures: {
-            experimentalObjectRestSpread: true,
-            jsx: true
-        },
-        sourceType: 'module'
-    },
-    plugins: ['react'],
-    rules: {
-        'react/jsx-uses-vars': ['error'],
-        'react/jsx-uses-react': ['error'],
-        'no-console': 0,
-        'indent': ['error', 4],
-        'linebreak-style': ['error', 'unix'],
-        'quotes': ['error', 'single'],
-        'quote-props': ['error', 'consistent-as-needed'],
-        'semi': ['error', 'always'],
-        'no-unused-vars': [
-            'error',
-            { vars: 'all', args: 'after-used', ignoreRestSiblings: true }
-        ]
-    }
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    "react/jsx-uses-vars": ["error"],
+    "react/jsx-uses-react": ["error"],
+    "no-console": 0,
+    indent: ["error", 4],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    "quote-props": ["error", "consistent-as-needed"],
+    semi: ["error", "always"],
+    "no-unused-vars": [
+      "error",
+      { vars: "all", args: "after-used", ignoreRestSiblings: true }
+    ]
+  }
 };
